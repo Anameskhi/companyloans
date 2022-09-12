fcb7080fcb837c0dfacd04daeac7dbea0238ecdc
Anameskhi <ameskhi@unisens.ge>
2022-09-12 11:24:42 +0400

    added avo gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount Avo::Engine, at: Avo.configuration.root_path
+  root 'home#index'
+  get '/avo', to: 'home#index'
 end
